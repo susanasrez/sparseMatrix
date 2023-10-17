e9b8e3f90a2f58fae51bee7e1a4bdee735987c13
Susana Suárez <susanasuarezmendoza26@gmail.com>
2023-10-17 15:34:57 +0100

    v1.3

--- src/main/java/org/example/controller/COOMatrixGenerator.java
+++ src/main/java/org/example/controller/COOMatrixGenerator.java
@@ -1,4 +1,57 @@
 package org.example.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class COOMatrixGenerator {
+
+    public static Map<String, Object> generateRandomCOOMatrix(int n, double density) {
+        if (density < 0.0 || density > 1.0) {
+            throw new IllegalArgumentException("Density must be in the range [0, 1].");
+        }
+
+        ArrayList<Integer> cooRows = new ArrayList<>();
+        ArrayList<Integer> cooColumns = new ArrayList<>();
+        ArrayList<Double> cooValues = new ArrayList<>();
+
+        Random random = new Random();
+        int totalNonZeroElements = (int) Math.round(n * n * density);
+
+        for (int i = 0; i < totalNonZeroElements; i++) {
+            int row = 0, col = 0;
+            boolean isUnique = false;
+            while (!isUnique) {
+                row = random.nextInt(n) + 1;
+                col = random.nextInt(n) + 1;
+                isUnique = !cooRows.contains(row) && !cooColumns.contains(col);
+            }
+
+            double value = random.nextDouble();
+            cooRows.add(row);
+            cooColumns.add(col);
+            cooValues.add(value);
+        }
+
+        Map<String, Object> cooMatrix = new HashMap<>();
+        cooMatrix.put("Rows", cooRows);
+        cooMatrix.put("Columns", cooColumns);
+        cooMatrix.put("Values", cooValues);
+
+        return cooMatrix;
+    }
+
+    public static void showResult(Map<String,Object> cooMatrix){
+        ArrayList<Integer> cooRows = (ArrayList<Integer>) cooMatrix.get("Rows");
+        ArrayList<Integer> cooColumns = (ArrayList<Integer>) cooMatrix.get("Columns");
+        ArrayList<Double> cooValues = (ArrayList<Double>) cooMatrix.get("Values");
+
+        for (int i = 0; i < cooRows.size(); i++) {
+            int row = cooRows.get(i);
+            int col = cooColumns.get(i);
+            double value = cooValues.get(i);
+            System.out.println("Row: " + row + ", Col: " + col + ", Value: " + value);
+        }
+    }
 }
--- src/main/java/org/example/controller/Controller.java
+++ src/main/java/org/example/controller/Controller.java
@@ -5,6 +5,7 @@ import org.example.model.MatrixMarketReader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class Controller {
 
@@ -13,14 +14,17 @@ public class Controller {
         //MatrixMarketReader matrixMarketReader = new MatrixMarketReader(path);
         //matrixMarketReader.readMatrixMarketFile();
         //matrixMarketReader.showMatrix();
-        ArrayList<Integer> cooRows = new ArrayList<>(Arrays.asList(1, 1, 1, 2 , 3, 3));
+
+        Map<String,Object> xd = COOMatrixGenerator.generateRandomCOOMatrix(4, 0.2);
+        COOMatrixGenerator.showResult(xd);
+        /*ArrayList<Integer> cooRows = new ArrayList<>(Arrays.asList(1, 1, 1, 2 , 3, 3));
         ArrayList<Integer> cooColumns = new ArrayList<>(Arrays.asList(1, 2, 3, 2, 2, 3));
         ArrayList<Double> cooValues = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 5.0, 8.0, 9.0));
 
 
         CompressorCRS compressor = new CompressorCRS(cooRows, cooColumns,cooValues,
                 3, 3);
-        compressor.printCRS();
+        compressor.printCRS();*/
 
 
     }
